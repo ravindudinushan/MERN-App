470197cb8190cac95d8fd3ae15d4ea6a2b50c05c
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-29 21:04:30 +0530

    CRUD

--- server/controller/ProductController.js
+++ server/controller/ProductController.js
@@ -15,12 +15,46 @@ const ProductController = {
         try {
             const productData = req.body;
             const product = await Product.create(productData);
+            res.status(201).json(product);
+        }catch (err){
+            console.error(err);
+            res.status(500).json({error: 'Something went wrong..!'})
+        }
+    },
+
+    getProduct: async function(req, res, next){
+        try {
+            const productId = req.params.id;
+            const product = await Product.findOne({id: productId})
             res.status(200).json(product);
         }catch (err){
             console.error(err);
             res.status(500).json({error: 'Something went wrong..!'})
         }
-    }
+    },
+
+    deleteProduct: async function(req, res, next){
+        try {
+            const productId = req.params.id;
+            const product = await Product.findOneAndDelete({id: productId})
+            res.status(203).json(product);
+        }catch (err){
+            console.error(err);
+            res.status(500).json({error: 'Something went wrong..!'})
+        }
+    },
+
+    updateProduct: async function(req, res, next){
+        try {
+            const productId = req.params.id;
+            const productData = req.body;
+            const product = await Product.findOneAndUpdate({id: productId},productData);
+            res.status(201).json(product);
+        }catch (err){
+            console.error(err);
+            res.status(500).json({error: 'Something went wrong..!'})
+        }
+    },
 }
 
 module.exports = ProductController;
--- server/routes/ProductRoutes.js
+++ server/routes/ProductRoutes.js
@@ -4,6 +4,9 @@ let ProductController = require('../controller/ProductController');
 
 /* GET users listing. */
 router.get('/all', ProductController.getAllProducts);
-router.post('/save', ProductController.saveProduct)
+router.post('/save', ProductController.saveProduct);
+router.get('/:id', ProductController.getProduct);
+router.delete('/:id', ProductController.deleteProduct);
+router.put('/update/:id', ProductController.updateProduct);
 
 module.exports = router;
